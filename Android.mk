5df6b332c83d1988028348f0c3afa4d462085096
Christopher Wiley <wiley@google.com>
2016-06-10 09:25:49 -0700

    Re-organize wificond makefile
    All tests are now run on the device (unittest and integration).
    Stop compiling host side versions of our static libraries.
    Move IPC related source into a dedicated static library.
      - integration tests need to be able to speak to wificond
      - integration tests should not be linked against wificond internal
        logic
    Move server.cpp into the static library implementing wificond.
    
    Bug: 28867751
    Test: Compiles, unittests still pass
    
    Change-Id: Ibe118576dbc09b309d589e5a015941428e3096b7

--- Android.mk
+++ Android.mk
@@ -15,10 +15,6 @@
 LOCAL_PATH := $(call my-dir)
 
 wificond_cpp_flags := -std=c++11 -Wall -Werror -Wno-unused-parameter
-wificond_cpp_src := looper_backed_event_loop.cpp
-
-wificond_test_util_cpp_src := \
-    tests/shell_utils.cpp \
 
 ###
 ### wificond daemon.
@@ -27,15 +23,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := wificond
 LOCAL_CPPFLAGS := $(wificond_cpp_flags)
 LOCAL_INIT_RC := wificond.rc
-LOCAL_AIDL_INCLUDES += $(LOCAL_PATH)/aidl
 LOCAL_SRC_FILES := \
-    aidl/android/net/wifi/IApInterface.aidl \
-    aidl/android/net/wifi/IChip.aidl \
-    aidl/android/net/wifi/IChipCallback.aidl \
-    aidl/android/net/wifi/IClientInterface.aidl \
-    aidl/android/net/wifi/IWificond.aidl \
-    main.cpp \
-    server.cpp
+    main.cpp
 LOCAL_SHARED_LIBRARIES := \
     libbinder \
     libbase \
@@ -50,24 +39,32 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwificond
 LOCAL_CPPFLAGS := $(wificond_cpp_flags)
-LOCAL_SRC_FILES := $(wificond_cpp_src)
+LOCAL_SRC_FILES := \
+    looper_backed_event_loop.cpp \
+    server.cpp
 LOCAL_SHARED_LIBRARIES := \
     libbase \
     libutils
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libwificond_ipc
 include $(BUILD_STATIC_LIBRARY)
 
 ###
-### host version of the wificond static library (for use in tests)
+### wificond IPC interface library
 ###
 include $(CLEAR_VARS)
-LOCAL_MODULE := libwificond_host
+LOCAL_MODULE := libwificond_ipc
+LOCAL_AIDL_INCLUDES += $(LOCAL_PATH)/aidl
 LOCAL_CPPFLAGS := $(wificond_cpp_flags)
-LOCAL_SRC_FILES := $(wificond_cpp_src)
-LOCAL_STATIC_LIBRARIES := \
-    libbase \
-    libutils
-LOCAL_MODULE_HOST_OS := linux
-include $(BUILD_HOST_STATIC_LIBRARY)
+LOCAL_SRC_FILES := \
+    aidl/android/net/wifi/IApInterface.aidl \
+    aidl/android/net/wifi/IChip.aidl \
+    aidl/android/net/wifi/IChipCallback.aidl \
+    aidl/android/net/wifi/IClientInterface.aidl \
+    aidl/android/net/wifi/IWificond.aidl
+LOCAL_SHARED_LIBRARIES := \
+    libbinder
+include $(BUILD_STATIC_LIBRARY)
 
 ###
 ### test util library
@@ -75,44 +72,30 @@ include $(BUILD_HOST_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwificond_test_utils
 LOCAL_CPPFLAGS := $(wificond_cpp_flags)
-LOCAL_SRC_FILES := $(wificond_test_util_cpp_src)
+LOCAL_SRC_FILES := \
+    tests/shell_utils.cpp
 LOCAL_SHARED_LIBRARIES := \
     libbase
 include $(BUILD_STATIC_LIBRARY)
 
 ###
-### host version of test util library
-###
-include $(CLEAR_VARS)
-LOCAL_MODULE := libwificond_test_utils_host
-LOCAL_MODULE_HOST_OS := linux
-LOCAL_CPPFLAGS := $(wificond_cpp_flags)
-LOCAL_LDLIBS_linux := -lrt
-LOCAL_SRC_FILES := $(wificond_test_util_cpp_src)
-LOCAL_STATIC_LIBRARIES := \
-    libbase
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-###
-### wificond host unit tests.
+### wificond unit tests.
 ###
 include $(CLEAR_VARS)
 LOCAL_MODULE := wificond_unit_test
 LOCAL_CPPFLAGS := $(wificond_cpp_flags)
-LOCAL_LDLIBS_linux := -lrt
 LOCAL_SRC_FILES := \
     tests/main.cpp \
     tests/looper_backed_event_loop_unittest.cpp \
     tests/shell_unittest.cpp
 LOCAL_STATIC_LIBRARIES := \
-    libgmock_host \
-    libwificond_host \
-    libwificond_test_utils_host \
+    libgmock \
+    libwificond \
+    libwificond_test_utils \
     libbase \
     libutils \
     liblog
-LOCAL_MODULE_HOST_OS := linux
-include $(BUILD_HOST_NATIVE_TEST)
+include $(BUILD_NATIVE_TEST)
 
 ###
 ### wificond device integration tests.
