335015939hi/dumbos-platform_system_connectivity_wificond

Add interfaces for getting available channels to WifiScannerImpl
This adds the following three aidl interfaces to wificond ScannerImpl:

int[] getAvailable2gChannels();
int[] getAvailable5gNonDFSChannels();
int[] getAvailableDFSChannels();

This also adds the corresponding integration tests.

Bug: 33297764
Test: compile, unit tests, integration tests

Change-Id: Idb14740769aa2ab368f311ae099b53005382a007

scanning/scanner_impl.cpp
@@ -22,6 +22,9 @@
 
 #include "wificond/scanning/scan_utils.h"
 
+using android::binder::Status;
+using std::vector;
+
 namespace android {
 namespace wificond {
 
@@ -39,6 +42,42 @@ ScannerImpl::ScannerImpl(uint32_t interface_index,
 }
 
 ScannerImpl::~ScannerImpl() {
+}
+
+bool ScannerImpl::CheckIsValid() {
+  if (!valid_) {
+    LOG(ERROR) << "Calling on a invalid scanner object."
+               << "Underlying client interface object was destroyed.";
+  }
+  return valid_;
+}
+
+Status ScannerImpl::getAvailable2gChannels(vector<int32_t>* out_frequencies) {
+  if (!CheckIsValid()) {
+    return Status::ok();
+  }
+  *out_frequencies = vector<int32_t>(band_info_.band_2g.begin(),
+                                     band_info_.band_2g.end());
+  return Status::ok();
+}
+
+Status ScannerImpl::getAvailable5gNonDFSChannels(
+    vector<int32_t>* out_frequencies) {
+  if (!CheckIsValid()) {
+    return Status::ok();
+  }
+  *out_frequencies = vector<int32_t>(band_info_.band_5g.begin(),
+                                     band_info_.band_5g.end());
+  return Status::ok();
+}
+
+Status ScannerImpl::getAvailableDFSChannels(vector<int32_t>* out_frequencies) {
+  if (!CheckIsValid()) {
+    return Status::ok();
+  }
+  *out_frequencies = vector<int32_t>(band_info_.band_dfs.begin(),
+                                     band_info_.band_dfs.end());
+  return Status::ok();
 }
 
 }  // namespace wificond

tests/integration/scanner_test.cpp
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+
+#include <android-base/logging.h>
+#include <gtest/gtest.h>
+#include <utils/StrongPointer.h>
+
+#include "android/net/wifi/IClientInterface.h"
+#include "android/net/wifi/IWificond.h"
+#include "wificond/tests/integration/process_utils.h"
+
+using android::net::wifi::IClientInterface;
+using android::net::wifi::IWificond;
+using android::net::wifi::IWifiScannerImpl;
+using android::wificond::tests::integration::ScopedDevModeWificond;
+using std::vector;
+
+namespace android {
+namespace wificond {
+namespace {
+
+sp<IWifiScannerImpl> InitInterfaceAndReturnScanner(sp<IWificond> service) {
+  sp<IWifiScannerImpl> scanner;
+  sp<IClientInterface> client_interface;
+  if (!service->createClientInterface(&client_interface).isOk()) {
+    LOG(FATAL) << "Failed to create client interface";
+    return nullptr;
+  }
+  if (client_interface == nullptr) {
+    LOG(FATAL) << "Failed to get a valid client interface handler";
+    return nullptr;
+  }
+  if (!client_interface->getWifiScannerImpl(&scanner).isOk()) {
+    LOG(FATAL) << "Failed to get a WifiScannerImpl handler";
+    return nullptr;
+  }
+  return scanner;
+}
+
+}  // namespace
+
+TEST(ScannerTest, CanGetValidWifiScannerImpl) {
+  ScopedDevModeWificond dev_mode;
+  sp<IWificond> service = dev_mode.EnterDevModeOrDie();
+  sp<IWifiScannerImpl> scanner = InitInterfaceAndReturnScanner(service);
+  ASSERT_NE(nullptr, scanner.get());
+}
+
+TEST(ScannerTest, CanGetAvailableChannels) {
+  ScopedDevModeWificond dev_mode;
+  sp<IWificond> service = dev_mode.EnterDevModeOrDie();
+  sp<IWifiScannerImpl> scanner = InitInterfaceAndReturnScanner(service);
+  ASSERT_NE(nullptr, scanner.get());
+
+  vector<int32_t> freqs_2g;
+  ASSERT_TRUE(scanner->getAvailable2gChannels(&freqs_2g).isOk());
+  ASSERT_TRUE(freqs_2g.size() != 0);
+
+  vector<int32_t> freqs_5g;
+  ASSERT_TRUE(scanner->getAvailable5gNonDFSChannels(&freqs_5g).isOk());
+  ASSERT_TRUE(freqs_5g.size() != 0);
+
+  vector<int32_t> freqs_dfs;
+  ASSERT_TRUE(scanner->getAvailableDFSChannels(&freqs_dfs).isOk());
+  // DFS support should be enabled explicitly, so we don't expect a non-empty
+  // DFS frequency list here.
+}
+
+}  // namespace wificond
+}  // namespace android

phhusson/platform_system_connectivity_wificond

scanning/scanner_impl.cpp
@@ -22,6 +22,9 @@
 
 #include "wificond/scanning/scan_utils.h"
 
+using android::binder::Status;
+using std::vector;
+
 namespace android {
 namespace wificond {
 
@@ -39,6 +42,42 @@ ScannerImpl::ScannerImpl(uint32_t interface_index,
 }
 
 ScannerImpl::~ScannerImpl() {
+}
+
+bool ScannerImpl::CheckIsValid() {
+  if (!valid_) {
+    LOG(DEBUG) << "Calling on a invalid scanner object."
+               << "Underlying client interface object was destroyed.";
+  }
+  return valid_;
+}
+
+Status ScannerImpl::getAvailable2gChannels(vector<int32_t>* out_frequencies) {
+  if (!CheckIsValid()) {
+    return Status::ok();
+  }
+  *out_frequencies = vector<int32_t>(band_info_.band_2g.begin(),
+                                     band_info_.band_2g.end());
+  return Status::ok();
+}
+
+Status ScannerImpl::getAvailable5gNonDFSChannels(
+    vector<int32_t>* out_frequencies) {
+  if (!CheckIsValid()) {
+    return Status::ok();
+  }
+  *out_frequencies = vector<int32_t>(band_info_.band_5g.begin(),
+                                     band_info_.band_5g.end());
+  return Status::ok();
+}
+
+Status ScannerImpl::getAvailableDFSChannels(vector<int32_t>* out_frequencies) {
+  if (!CheckIsValid()) {
+    return Status::ok();
+  }
+  *out_frequencies = vector<int32_t>(band_info_.band_dfs.begin(),
+                                     band_info_.band_dfs.end());
+  return Status::ok();
 }
 
 }  // namespace wificond

tests/integration/scanner_test.cpp
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+
+#include <android-base/logging.h>
+#include <gtest/gtest.h>
+#include <utils/StrongPointer.h>
+
+#include "android/net/wifi/IClientInterface.h"
+#include "android/net/wifi/IWificond.h"
+#include "wificond/tests/integration/process_utils.h"
+
+using android::net::wifi::IClientInterface;
+using android::net::wifi::IWificond;
+using android::net::wifi::IWifiScannerImpl;
+using android::wificond::tests::integration::ScopedDevModeWificond;
+using std::vector;
+
+namespace android {
+namespace wificond {
+namespace {
+
+sp<IWifiScannerImpl> InitInterfaceAndReturnScanner(sp<IWificond> service) {
+  sp<IWifiScannerImpl> scanner;
+  sp<IClientInterface> client_interface;
+  if (!service->createClientInterface(&client_interface).isOk()) {
+    LOG(FATAL) << "Failed to create client interface";
+    return nullptr;
+  }
+  if (client_interface == nullptr) {
+    LOG(FATAL) << "Failed to get a valid client interface handler";
+    return nullptr;
+  }
+  if (!client_interface->getWifiScannerImpl(&scanner).isOk()) {
+    LOG(FATAL) << "Failed to get a WifiScannerImpl handler";
+    return nullptr;
+  }
+  return scanner;
+}
+
+}  // namespace
+
+TEST(ScannerTest, CanGetValidWifiScannerImpl) {
+  ScopedDevModeWificond dev_mode;
+  sp<IWificond> service = dev_mode.EnterDevModeOrDie();
+  EXPECT_NE(nullptr, InitInterfaceAndReturnScanner(service).get());
+}
+
+TEST(ScannerTest, CanGetAvailableChannels) {
+  ScopedDevModeWificond dev_mode;
+  sp<IWificond> service = dev_mode.EnterDevModeOrDie();
+  sp<IWifiScannerImpl> scanner = InitInterfaceAndReturnScanner(service);
+  ASSERT_NE(nullptr, scanner.get());
+
+  vector<int32_t> freqs_2g;
+  ASSERT_TRUE(scanner->getAvailable2gChannels(&freqs_2g).isOk());
+  EXPECT_TRUE(freqs_2g.size() != 0);
+
+  vector<int32_t> freqs_5g;
+  ASSERT_TRUE(scanner->getAvailable5gNonDFSChannels(&freqs_5g).isOk());
+  EXPECT_TRUE(freqs_5g.size() != 0);
+
+  vector<int32_t> freqs_dfs;
+  ASSERT_TRUE(scanner->getAvailableDFSChannels(&freqs_dfs).isOk());
+  // DFS support should be enabled explicitly, so we don't expect a non-empty
+  // DFS frequency list here.
+}
+
+}  // namespace wificond
+}  // namespace android