335015939hi/dumbos-platform_system_connectivity_wificond

Add support of randomized mac address to wificond ScanUtils
Bug: 33218723
Change-Id: I81e3507f4cc46894b8ae35f0df0814006ddc1ead
Test: compile

scanning/scanner_impl.cpp
@@ -113,7 +113,8 @@ Status ScannerImpl::scan(const vector<int32_t>& freqs,
     raw_ssids.emplace_back(vector<uint8_t>(ssid_str.begin(), ssid_str.end()));
   }
   vector<uint32_t> freqs_converted(freqs.begin(), freqs.end());
-  if (!scan_utils_->Scan(interface_index_, raw_ssids, freqs_converted)) {
+  bool random_mac =  wiphy_features_.supports_random_mac_oneshot_scan;
+  if (!scan_utils_->Scan(interface_index_, random_mac, raw_ssids, freqs_converted)) {
     *out_success = false;
     LOG(ERROR) << "Failed to start scan";
     return Status::ok();

phhusson/platform_system_connectivity_wificond

scanning/scanner_impl.cpp
@@ -107,8 +107,11 @@ Status ScannerImpl::scan(const SingleScanSettings& scan_settings,
   if (!CheckIsValid()) {
     return Status::ok();
   }
+
+  bool random_mac =  wiphy_features_.supports_random_mac_oneshot_scan;
+
   if (scan_settings.is_full_scan_) {
-    if (!scan_utils_->StartFullScan(interface_index_)) {
+    if (!scan_utils_->StartFullScan(interface_index_, random_mac)) {
       *out_success = false;
       return Status::ok();
     }
@@ -124,7 +127,7 @@ Status ScannerImpl::scan(const SingleScanSettings& scan_settings,
     freqs.push_back(channel.frequency_);
   }
 
-  if (!scan_utils_->Scan(interface_index_, ssids, freqs)) {
+  if (!scan_utils_->Scan(interface_index_, random_mac, ssids, freqs)) {
     *out_success = false;
     LOG(ERROR) << "Failed to start a scan";
     return Status::ok();