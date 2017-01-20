335015939hi/dumbos-platform_system_connectivity_wificond

Add scan result notification interfaces to wificond scanner
Bug: 33398008
Test: compile

Change-Id: I10eb85b67be4b5b1829c006f92e0cc4d37a9be7d

scanning/scanner_impl.h
@@ -54,10 +54,18 @@ class ScannerImpl : public android::net::wifi::BnWifiScannerImpl {
       const ::std::vector<int32_t>& freqs,
       const ::std::vector<::android::String16>& ssids,
       bool* out_success) override;
+  ::android::binder::Status SubscribeScanResultReadyNotification(
+      const ::android::sp<::android::net::wifi::IScanEvent>& handler) override;
+  ::android::binder::Status UnsubscribeScanResultReadyNotification() override;
   void Invalidate() { valid_ = false; }
 
  private:
   bool CheckIsValid();
+  void OnScanResultsReady(
+      uint32_t interface_index,
+      bool aborted,
+      std::vector<std::vector<uint8_t>>& ssids,
+      std::vector<uint32_t>& frequencies);
 
   bool valid_;
   uint32_t interface_index_;
@@ -68,6 +76,7 @@ class ScannerImpl : public android::net::wifi::BnWifiScannerImpl {
   const WiphyFeatures wiphy_features_;
 
   ScanUtils* scan_utils_;
+  ::android::sp<::android::net::wifi::IScanEvent> scan_event_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(ScannerImpl);
 };

phhusson/platform_system_connectivity_wificond

scanning/scanner_impl.h
@@ -55,10 +55,18 @@ class ScannerImpl : public android::net::wifi::BnWifiScannerImpl {
       const ::com::android::server::wifi::wificond::SingleScanSettings&
           scan_settings,
       bool* out_success) override;
+  ::android::binder::Status SubscribeScanEvents(
+      const ::android::sp<::android::net::wifi::IScanEvent>& handler) override;
+  ::android::binder::Status UnsubscribeScanEvents() override;
   void Invalidate() { valid_ = false; }
 
  private:
   bool CheckIsValid();
+  void OnScanResultsReady(
+      uint32_t interface_index,
+      bool aborted,
+      std::vector<std::vector<uint8_t>>& ssids,
+      std::vector<uint32_t>& frequencies);
 
   bool valid_;
   uint32_t interface_index_;
@@ -69,6 +77,7 @@ class ScannerImpl : public android::net::wifi::BnWifiScannerImpl {
   const WiphyFeatures wiphy_features_;
 
   ScanUtils* scan_utils_;
+  ::android::sp<::android::net::wifi::IScanEvent> scan_event_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(ScannerImpl);
 };